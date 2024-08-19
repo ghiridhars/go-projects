da1088303f8e467e3f5215c9e8861baaa55a2c25
Ghiridhar <ghiridhars@gmail.com>
2024-08-19 17:21:35 +0530

    fixed postgres connection issues and externalized db values

--- basic/src/handlers.go
+++ basic/src/handlers.go
@@ -3,12 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log/slog"
+	_ "log/slog"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // get all items
 func getItems(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT id, name FROM items")
+	slog.Info("get all items")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -51,4 +56,54 @@ func createItem(w http.ResponseWriter, r *http.Request) {
 	newItem.ID = fmt.Sprint("%d", id)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(newItem)
+}
+
+// Update an item by ID
+func updateItem(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var updatedItem Item
+	if err := json.NewDecoder(r.Body).Decode(&updatedItem); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if updatedItem.Name == "" {
+		http.Error(w, "Name is required", http.StatusBadRequest)
+		return
+	}
+
+	result, err := db.Exec("UPDATE items SET name = $1 WHERE id = $2", updatedItem.Name, params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil || rowsAffected == 0 {
+		http.Error(w, "Item not found", http.StatusNotFound)
+		return
+	}
+
+	updatedItem.ID = params["id"]
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedItem)
+}
+
+func deleteItem(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	result, err := db.Exec("DELETE FROM items WHERE id = $1", params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil || rowsAffected == 0 {
+		http.Error(w, "Item not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"result": "success"})
 }
--- basic/src/main.go
+++ basic/src/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -15,10 +16,18 @@ var db *sql.DB
 
 func initDB() {
 	var err error
-	connStr := "postgres://youruser:yourpassword@localhost:5432/yourdb?sslmode=disable"
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	// "postgres://ghiri:develop@postgres-go:5432/items?sslmode=disable"
+	slog.Info(connStr)
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
+	}
+	slog.Info("success")
+
+	if err = db.Ping(); err != nil {
+		slog.Error("Postgres ping error : (%v)", err)
 	}
 
 	// create items if not there
@@ -28,42 +37,27 @@ func initDB() {
         name TEXT NOT NULL
     );`
 	_, err = db.Exec(query)
+
+	slog.Info("SUccess")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to my first GO page")
-}
-
-func getitems(w http.ResponseWriter, r *http.Request) {
-	rows, err := db.Query("SELECT * from items")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	var items []Item
-	for rows.Next() {
-		var item Item
-		err := rows.Scan(&item.ID, &item.Name)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		items = append(items, item)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(items)
+	fmt.Fprint(w, "Welcome to my GO page")
 }
 
 func main() {
-	// initDB()
+	initDB()
 
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", homePage)
-	router.HandleFunc("/items", getitems).Methods("GET")
-	router.HandleFunc("/items", createItem).Methods("POST")
+	router.HandleFunc("/items", getItems).Methods("GET")
+	router.HandleFunc("/createItem", createItem).Methods("POST")
+	router.HandleFunc("/deleteItem/{id}", deleteItem).Methods("DELETE")
+	router.HandleFunc("/udpateItem/{id}", updateItem).Methods("PUT")
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
